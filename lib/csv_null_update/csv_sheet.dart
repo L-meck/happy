36d0e97b8a1dbd2eb00b798d3072cb4c38c562d3
L-meck <Kanaidavid3@gmail.com>
2022-05-17 20:48:44 +0300

    converted dependency package to null safety

--- lib/csv_null_update/csv_sheet.dart
+++ lib/csv_null_update/csv_sheet.dart
@@ -4,16 +4,17 @@ import 'dart:collection' show HashMap;
 
 part 'csv_column.dart';
 part 'csv_row.dart';
+
 /// An indexable collection of CSV Values as a spreadsheet.
-/// 
-/// As spreadsheets (and unlike lists), a [CsvSheet] is indexed via 
+///
+/// As spreadsheets (and unlike lists), a [CsvSheet] is indexed via
 /// column and then row, rather than row and column.
-/// 
+///
 /// Columns can be referenced via their index number (1-based) or
 /// via their column header [String].
-/// 
+///
 /// CsvSheets can be visualized as such:
-/// 
+///
 ///          1    2    3
 ///       .----+----+----.
 ///       |col1|col2|col3|
@@ -24,83 +25,82 @@ part 'csv_row.dart';
 ///       +----+----+----+
 ///     3 |  7 |  8 |  9 |
 ///       '----+----+----'
-/// 
+///
 ///     // access the value of cell 2,3 (in a spreadsheet as B3)
 ///     var value = csvSheet[2][3]; // 8
-///     
+///
 ///     // If the cell has the header value of 'col1'
 ///     var value = csvSheet['col1'][2]; // 4
 class CsvSheet {
-  List _contents;
-  HashMap _headers;
-  _CsvColumn _fakeColumn;
-  CsvRow _row;
-  
+  List? _contents;
+  HashMap? _headers;
+  _CsvColumn? _fakeColumn;
+  CsvRow? _row;
+
   /// Return true if this sheet was created with a header row.
-  bool hasHeaderRow;
-  
-  /// Creates a CSV Sheet with the given contents. 
-  /// 
+  bool? hasHeaderRow;
+
+  /// Creates a CSV Sheet with the given contents.
+  ///
   /// If headerRow is provided indicates that the first row of contents is a
   /// header and not part of the data. Default is [false].
-  /// 
+  ///
   /// fieldSep is an optional field seperator if values are not comma separated.
-  /// 
+  ///
   ///     // tab separated sheet can be parsed with:
   ///     var sheet = new CsvSheet(contents, fieldSep: '\t');
-  /// 
+  ///
   /// If you're using a file format which does not end lines with a simple '\n'
   /// you can optionally specify what terminator rows are deliminated with.
-  /// 
+  ///
   ///     // Usually windows style should automatically be detected anyways.
   ///     var sheet = new CsvSheet(contents, lineSep: '\r\n');
-  CsvSheet(String contents, 
-      { bool headerRow= false,
-      String fieldSep= ',',
-      String lineSep = '\n' }) {
-    _fakeColumn =  _CsvColumn(this);
-    
+  ///
+
+  CsvSheet(String contents,
+      {bool headerRow = false, String fieldSep = ',', String lineSep = '\n'}) {
+    _fakeColumn = _CsvColumn(this);
+
     var allRows = contents.split(lineSep);
-    hasHeaderRow = headerRow;
-    
-    if(hasHeaderRow) {
+    var hasHeaderRow = headerRow;
+
+    if (hasHeaderRow) {
       var _rows = allRows[0].split(fieldSep);
       _headers = HashMap();
-      for(var i = 0; i < _rows.length; i ++) {
+      for (var i = 0; i < _rows.length; i++) {
         var val = _rows[i].replaceAll('"', '');
-        if(_rows.indexOf(val, i+1) != -1) {
-          throw 
-             FormatException('The header column $val appears more than once');
+        if (_rows.indexOf(val, i + 1) != -1) {
+          throw FormatException(
+              'The header column $val appears more than once');
         }
-        _headers[val] = i;
+        _headers![val] = i;
       }
-      _row = CsvRow(_headers);
+      _row = CsvRow(_headers!, _rows);
       _contents = <List<String>>[];
       var allRowsLength = allRows.length;
-      for(var index = 1; index < allRowsLength; index++) {
-        
+      for (var index = 1; index < allRowsLength; index++) {
         // Find unterminated quotes per row
         // usually occurs if there are multiple line entries.
         var numQuotes = _countQuotes(allRows[index]);
-        while(numQuotes % 2 != 0) {
-          if((index + 1) > (allRowsLength - 1)) {
+        while (numQuotes % 2 != 0) {
+          if ((index + 1) > (allRowsLength - 1)) {
             throw const FormatException('Unterminated quoted field');
           }
-          
+
           allRows[index] = allRows[index] + lineSep + allRows[index + 1];
           allRows.removeAt(index + 1);
           allRowsLength -= 1;
           numQuotes = _countQuotes(allRows[index]);
         }
-        
+
         // Find unterminated quotes in each cell
         var tmpRow = allRows[index].split(fieldSep);
         var rowLength = tmpRow.length;
-        
-        for(var ind = 0; ind < rowLength; ind++) {
+
+        for (var ind = 0; ind < rowLength; ind++) {
           numQuotes = _countQuotes(tmpRow[ind]);
-          while(numQuotes % 2 == 1) {
-            if((ind + 1) > (rowLength - 1)){
+          while (numQuotes % 2 == 1) {
+            if ((ind + 1) > (rowLength - 1)) {
               throw const FormatException('Unterminated quoted field');
             }
             tmpRow[ind] = tmpRow[ind] + fieldSep + tmpRow[ind + 1];
@@ -110,20 +110,20 @@ class CsvSheet {
           }
           tmpRow[ind] = tmpRow[ind].replaceAll('"', '').trim();
         }
-            
-        _contents.add(tmpRow);
+        //  TODO: Recheck below Null Check
+        _contents!.add(tmpRow); //Edited by #lameck
       } // end for loop
-    } else { // hasHeaderRow
-      _row = new CsvRow();
+    } else {
+      // hasHeaderRow
+      _row = CsvRow(_headers!, allRows); //TODO: Recheck this file.
       _contents = <List<String>>[];
       var allRowsLength = allRows.length;
-      for(var index = 0; index < allRowsLength; index++) {
-        
+      for (var index = 0; index < allRowsLength; index++) {
         // Find unterminated quotes per row
         // usually occurs if there are multiple line entries.
         var numQuotes = _countQuotes(allRows[index]);
-        while(numQuotes % 2 != 0) {
-          if((index + 1) > (allRowsLength - 1)){
+        while (numQuotes % 2 != 0) {
+          if ((index + 1) > (allRowsLength - 1)) {
             throw const FormatException('Unterminated quoted field');
           }
           allRows[index] = allRows[index] + lineSep + allRows[index + 1];
@@ -131,15 +131,15 @@ class CsvSheet {
           allRowsLength -= 1;
           numQuotes = _countQuotes(allRows[index]);
         }
-        
+
         // Find unterminated quotes in each cell
         var tmpRow = allRows[index].split(fieldSep);
         var rowLength = tmpRow.length;
-        
-        for(var ind = 0; ind < rowLength; ind++) {
+
+        for (var ind = 0; ind < rowLength; ind++) {
           numQuotes = _countQuotes(tmpRow[ind]);
-          while(numQuotes % 2 == 1) {
-            if((ind + 1) > (rowLength - 1)){
+          while (numQuotes % 2 == 1) {
+            if ((ind + 1) > (rowLength - 1)) {
               throw const FormatException('Unterminated quoted field');
             }
             tmpRow[ind] = tmpRow[ind] + fieldSep + tmpRow[ind + 1];
@@ -149,71 +149,73 @@ class CsvSheet {
           }
           tmpRow[ind] = tmpRow[ind].replaceAll('"', '').trim();
         }
-            
-        _contents.add(tmpRow);
+
+        ///TODO: recheck all below code till end of below WHILE LOOP
+        _contents!.add(tmpRow);
       } // end for loop
     }
-    while(_contents.last.length != _contents.first.length) {
-      _contents.removeLast();
+    while (_contents!.last.length != _contents!.first.length) {
+      _contents!.removeLast();
     }
   }
-  
+
   /// Access the column specified by [index]. Index may be a 1-based integer
   /// value or a string matching one of the header rows.
-  /// 
+  ///
   /// Throws a [RangeError] if invalid index or if a String index is used but
   /// [hasHeaderRow] is false.
   operator [](index) {
-    if(index is String) {
+    if (index is String) {
       var tmp = index;
-      if(!hasHeaderRow) {
+      if (!hasHeaderRow!) {
         throw RangeError('$tmp is not a valid column header');
       }
-      
-      if(!_headers.containsKey(index)) throw RangeError('$tmp is not a valid column header');
-      index = _headers[index];
-      _fakeColumn.row = index;
+
+      if (!_headers!.containsKey(index))
+        throw RangeError('$tmp is not a valid column header');
+      index = _headers![index];
+      _fakeColumn!.row = index;
     } else {
-      _fakeColumn.row = index-1;
+      _fakeColumn!.row = index - 1;
     }
-    
+
     return _fakeColumn;
   }
-  
-  /// Iterate through each row in the spreadsheet as a CsvRow, calling 'action' 
+
+  /// Iterate through each row in the spreadsheet as a CsvRow, calling 'action'
   /// for each row. It is an error if action tries to modify the list.
-  /// 
+  ///
   /// The CsvRow is indexable via the headers if applicable.
-  void forEachRow(void Function(CsvRow cells) action) { 
-  // void forEachRow(void action(CsvRow cells)) {
-    for (var row in _contents) {
-    // for (var row in _contents) {
-      _row.row = row;
-      action(_row);
+  void forEachRow(void Function(CsvRow cells) action) {
+    // void forEachRow(void action(CsvRow cells)) {
+    for (var row in _contents!) {
+      // for (var row in _contents) {
+      _row!.row = row;
+      action(_row!);
     }
   }
-  
+
   /// Return the number of rows contained in this sheet. Does not included
   /// the header row, if provided.
-  int get numRows => _contents.length;
-  
+  int get numRows => _contents!.length;
+
   /// Return the number of columns contained in this sheet.
-  int get numCols => _contents[0].length;
-  
+  int get numCols => _contents![0].length;
+
   // Used by _CsvColumn to access rows spreadsheet style instead of list style.
-  _getValue(column, row) => _contents[row][column];
-  
+  _getValue(column, row) => _contents![row][column];
+
   num _countQuotes(String str) {
     var numQuotes = 0;
     var quoteIndex = str.indexOf('"');
-    while(quoteIndex != -1) {
+    while (quoteIndex != -1) {
       numQuotes += 1;
-      if((quoteIndex + 1) < str.length) {
+      if ((quoteIndex + 1) < str.length) {
         quoteIndex = str.indexOf('"', quoteIndex + 1);
       } else {
         break;
       }
     }
     return numQuotes;
-  } 
+  }
 }
